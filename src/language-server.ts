d5769445b9ea689dd7dbdd565323556838c9da2a
Kwang Yul Seo <kseo@kodebox.io>
2017-09-13 09:04:18 +0900

    Start up a cluster of worker processes
    Crashing workers are restarted automatically.

--- src/language-server.ts
+++ src/language-server.ts
@@ -1,15 +1,18 @@
 import { ServeOptions, serve } from "./server";
 const program = require("commander");
 
+const numCPUs = require("os").cpus().length;
 const packageJson = require("../package.json");
 const defaultLspPort = 2089;
 
 program
     .version(packageJson.version)
     .option("-p, --port [port]', 'specifies LSP port to use (" + defaultLspPort + ")", parseInt)
+    .option("-c, --cluster [num]", "number of concurrent cluster workers (defaults to number of CPUs, " + numCPUs + ")", parseInt)
     .parse(process.argv);
 
 const options: ServeOptions = {
+    clusterSize: program.cluster || numCPUs,
     lspPort: program.port || defaultLspPort
 };
 
--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,34 @@ export function createClusterLogger(logger = new StdioLogger()): Logger {
 
 /** Options to `serve()` */
 export interface ServeOptions extends MessageLogOptions {
+    /** Amount of workers to spawn */
+    clusterSize: number;
+
     /** Port to listen on for TCP LSP connections */
     lspPort: number;
 }
 
+/**
+ * Starts up a cluster of worker processes that listen on the same TCP socket.
+ * Crashing workers are restarted automatically.
+ *
+ * @param options
+ */
 export function serve(options: ServeOptions) {
     const logger = options.logger || createClusterLogger();
-    logger.info(`Listening for incoming LSP connections on ${options.lspPort}`);
+    if (options.clusterSize > 1 && cluster.isMaster) {
+        logger.log(`Spawning ${options.clusterSize} workers`);
+        cluster.on("online", worker => {
+            logger.log(`Worker ${worker.id} (PID ${worker.process.pid}) online`);
+        });
+        cluster.on("exit", (worker, code, signal) => {
+            logger.error(`Worker ${worker.id} (PID ${worker.process.pid}) exited from signal ${signal} with code ${code}, restarting`);
+            cluster.fork();
+        });
+        for (let i = 0; i < options.clusterSize; ++i) {
+            cluster.fork();
+        }
+    } else {
+        logger.info(`Listening for incoming LSP connections on ${options.lspPort}`);
+    }
 }
