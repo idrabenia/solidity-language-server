abd2ab510518e703a000ec1c1624c90c42f73168
Kwang Yul Seo <kseo@kodebox.io>
2017-09-13 09:16:11 +0900

    Add MessageEmitter class

--- src/connection.ts
+++ src/connection.ts
@@ -1,3 +1,7 @@
+import { EventEmitter } from "events";
+
+import { Message, StreamMessageReader as VSCodeStreamMessageReader } from "vscode-jsonrpc";
+
 import { Logger } from "./logging";
 
 export interface MessageLogOptions {
@@ -6,4 +10,56 @@ export interface MessageLogOptions {
 
     /** Whether to log all messages */
     logMessages?: boolean;
+}
+
+/**
+ * Takes a NodeJS ReadableStream and emits parsed messages received on the stream.
+ * In opposite to StreamMessageReader, supports multiple listeners and is compatible with Observables
+ */
+export class MessageEmitter extends EventEmitter {
+
+    constructor(input: NodeJS.ReadableStream, options: MessageLogOptions = {}) {
+        super();
+        const reader = new VSCodeStreamMessageReader(input);
+        // Forward events
+        reader.listen(msg => {
+            this.emit("message", msg);
+        });
+        reader.onError(err => {
+            this.emit("error", err);
+        });
+        reader.onClose(() => {
+            this.emit("close");
+        });
+        this.setMaxListeners(Infinity);
+        // Register message listener to log messages if configured
+        if (options.logMessages && options.logger) {
+            const logger = options.logger;
+            this.on("message", message => {
+                logger.log("-->", message);
+            });
+        }
+    }
+
+    /** Emitted when a new JSON RPC message was received on the input stream */
+    on(event: "message", listener: (message: Message) => void): this;
+    /** Emitted when the underlying input stream emitted an error */
+    on(event: "error", listener: (error: Error) => void): this;
+    /** Emitted when the underlying input stream was closed */
+    on(event: "close", listener: () => void): this;
+    /* istanbul ignore next */
+    on(event: string, listener: (...args: any[]) => void): this {
+        return super.on(event, listener);
+    }
+
+    /** Emitted when a new JSON RPC message was received on the input stream */
+    once(event: "message", listener: (message: Message) => void): this;
+    /** Emitted when the underlying input stream emitted an error */
+    once(event: "error", listener: (error: Error) => void): this;
+    /** Emitted when the underlying input stream was closed */
+    once(event: "close", listener: () => void): this;
+    /* istanbul ignore next */
+    once(event: string, listener: (...args: any[]) => void): this {
+        return super.on(event, listener);
+    }
 }
