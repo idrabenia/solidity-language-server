6296c56c398fe198b195473a7ce266aef6628bae
Kwang Yul Seo <kseo@kodebox.io>
2017-09-22 12:13:19 +0900

    Introduce SolidityCompiler class

---
+++ src/solidity-compiler.ts
@@ -0,0 +1,53 @@
+import {
+    Diagnostic,
+    DiagnosticSeverity
+} from "vscode-languageserver";
+
+const solc = require("solc");
+
+export class SolidityCompiler {
+    compile(path: string, text: string): Diagnostic[] {
+        const input = { [path]: text };
+        const output = this.compileContracts({ sources: input });
+        if (!output.errors) return [];
+        return output.errors.map(solcErrToDiagnostic);
+    }
+
+    private compileContracts(sources: any): { errors: string[] } {
+        // Setting 1 as second paramater activates the optimiser
+        return solc.compile(sources, 1);
+    }
+}
+
+function solcErrToDiagnostic(error: string): Diagnostic {
+    const errorSegments = error.split(":");
+    const line = parseInt(errorSegments[1]);
+    const column = parseInt(errorSegments[2]);
+    const severity = getDiagnosticSeverity(errorSegments[3]);
+
+    return {
+        message: error,
+        range: {
+            start: {
+                line: line - 1,
+                character: column
+            },
+            end: {
+                line: line - 1,
+                character: column
+            },
+        },
+        severity
+    };
+}
+
+function getDiagnosticSeverity(severity: string): DiagnosticSeverity {
+    switch (severity) {
+        case " Error":
+            return DiagnosticSeverity.Error;
+        case " Warning":
+            return DiagnosticSeverity.Warning;
+        default:
+            return DiagnosticSeverity.Error;
+    }
+}
