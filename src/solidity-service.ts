6cb206a7165462b75bf37df85ae16130ef176fac
Kwang Yul Seo <kseo@kodebox.io>
2017-09-30 13:47:39 +0900

    Add dummy initialized method

--- src/solidity-service.ts
+++ src/solidity-service.ts
@@ -107,6 +107,16 @@ export class SolidityService {
         } as Operation);
     }
 
+    /**
+     * The initialized notification is sent from the client to the server after the client received the
+     * result of the initialize request but before the client is sending any other request or notification
+     * to the server. The server can use the initialized notification for example to dynamically register
+     * capabilities.
+     */
+    async initialized(): Promise<void> {
+        // No op.
+    }
+
     protected _initializeFileSystems(): void {
         this.inMemoryFileSystem = new InMemoryFileSystem(this.root, this.logger);
     }
