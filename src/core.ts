fb7e62392538e5e697ba358458ad6ca9a53a46c2
Kwang Yul Seo <kseo@kodebox.io>
2017-10-03 17:49:33 +0900

    Update firstOrUndefined and lastOrUndefined

--- src/core.ts
+++ src/core.ts
@@ -113,21 +113,39 @@ export function getRootLength(path: string): number {
 }
 
 /**
+ * Gets the actual offset into an array for a relative offset. Negative offsets indicate a
+ * position offset from the end of the array.
+ */
+function toOffset(array: ReadonlyArray<any>, offset: number) {
+    return offset < 0 ? array.length + offset : offset;
+}
+
+/**
+ * Returns the element at a specific offset in an array if non-empty, `undefined` otherwise.
+ * A negative offset indicates the element should be retrieved from the end of the array.
+ */
+export function elementAt<T>(array: ReadonlyArray<T> | undefined, offset: number): T | undefined {
+    if (array) {
+        offset = toOffset(array, offset);
+        if (offset < array.length) {
+            return array[offset];
+        }
+    }
+    return undefined;
+}
+
+/**
  * Returns the first element of an array if non-empty, `undefined` otherwise.
  */
-export function firstOrUndefined<T>(array: T[]): T {
-    return (array && array.length > 0
-        ? array[0]
-        : undefined) as T;
+export function firstOrUndefined<T>(array: ReadonlyArray<T>): T | undefined {
+    return elementAt(array, 0);
 }
 
 /**
  * Returns the last element of an array if non-empty, `undefined` otherwise.
  */
-export function lastOrUndefined<T>(array: T[]): T {
-    return (array && array.length > 0
-        ? array[array.length - 1]
-        : undefined) as T;
+export function lastOrUndefined<T>(array: ReadonlyArray<T>): T | undefined {
+    return elementAt(array, -1);
 }
 
 /**
