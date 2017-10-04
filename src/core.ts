16ee0430b6047fd68792afb16729a84954a87f14
Kwang Yul Seo <kseo@kodebox.io>
2017-10-04 11:48:29 +0900

    Add forEach function

--- src/core.ts
+++ src/core.ts
@@ -600,3 +600,21 @@ declare const Map: { new <T>(): Map<T> } | undefined;
 export function createMap<T>(): Map<T> {
     return new Map<T>();
 }
+
+
+/**
+ * Iterates through 'array' by index and performs the callback on each element of array until the callback
+ * returns a truthy value, then returns that value.
+ * If no such value is found, the callback is applied to each element of array and undefined is returned.
+ */
+export function forEach<T, U>(array: ReadonlyArray<T> | undefined, callback: (element: T, index: number) => U | undefined): U | undefined {
+    if (array) {
+        for (let i = 0; i < array.length; i++) {
+            const result = callback(array[i], i);
+            if (result) {
+                return result;
+            }
+        }
+    }
+    return undefined;
+}
