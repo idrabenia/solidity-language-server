6403bd40843b2b0cbf5f3a36a025e3b58bb4686a
Kwang Yul Seo <kseo@kodebox.io>
2017-10-14 09:51:55 +0900

    Remove sayHello command

--- src/extension.ts
+++ src/extension.ts
@@ -18,18 +18,6 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "solidity-language-server" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand("extension.sayHello", () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage("Hello World!");
-    });
-
-    context.subscriptions.push(disposable);
-
     const serverModule = path.join(__dirname, "server", "languageServerIpc.js");
 
     const serverOptions: ServerOptions = {
